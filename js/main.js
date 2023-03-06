6f40845527dec1039ecae0ef8291ba9cfaf828e5
NicholasConti <conti94@live.it>
2023-03-06 15:32:52 +0100

    random array

---
+++ js/main.js
@@ -0,0 +1,34 @@
+"use strict";
+
+//FUNCTIONS
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function MyCreateRandom(arrayLenght, min, max){
+    const numList = [];
+
+    while (numList.length < arrayLenght) {
+        const listNumber = getRndInteger(min, max);
+
+        if (!numList.includes(listNumber)) {
+            numList.push(listNumber);
+        }
+    }
+    return numList;
+
+}
+
+
+
+//MAIN
+
+const nLength = 5;
+const nMax = 100;
+
+const RandomNumList = MyCreateRandom(nLength, 1, nMax);
+
+const printList = document.getElementById('random-numbers');
+
+printList.innerHTML = RandomNumList;
