d024ba1db96bc759afa14f4b9a64274d92e1b57a
NicholasConti <conti94@live.it>
2023-03-06 16:07:07 +0100

    input user w/setTimeout

--- js/main.js
+++ js/main.js
@@ -6,10 +6,10 @@ function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function MyCreateRandom(arrayLenght, min, max){
+function MyCreateRandom(arrayLength, min, max){
     const numList = [];
 
-    while (numList.length < arrayLenght) {
+    while (numList.length < arrayLength) {
         const listNumber = getRndInteger(min, max);
 
         if (!numList.includes(listNumber)) {
@@ -17,16 +17,27 @@ function MyCreateRandom(arrayLenght, min, max){
         }
     }
     return numList;
-
 }
 
+function showPrompt(arrayPromptLength, min, max){
+    let inputsUser = [];
+    while (inputsUser.length < arrayPromptLength){
+        const inputNumber = prompt(`Inserisci un numero da ${min} a ${max}`);
+        if ((!inputsUser.includes(inputNumber)) && (inputNumber <= max)){
+            inputsUser.push(inputNumber);
+        }
+    }
+    console.log(inputsUser);
 
+    return inputsUser;
+}
 
 //MAIN
 
 const nLength = 5;
 const nMax = 100;
 const timeToSee = 5;
+const timeToSee2 = timeToSee + 1;
 
 const RandomNumList = MyCreateRandom(nLength, 1, nMax);
 
@@ -37,3 +48,7 @@ printList.innerHTML = RandomNumList;
 setTimeout(function(){
     printList.innerHTML = '';
 }, timeToSee * 1000);
+
+setTimeout(showPrompt, timeToSee2 * 1000, nLength, 1, nMax);
+
+
